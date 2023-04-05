5ef898a8a8b38f7ad706a219d35f0b3ed0f094ed
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-05 15:56:07 -0300

    feat: input calculate area

--- src/main/java/com/trybe/acc/java/Area.java
+++ src/main/java/com/trybe/acc/java/Area.java
@@ -3,19 +3,22 @@ package com.trybe.acc.java;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * Class Area.
+ */
 public class Area {
-   public static void main(String[] args) {
-    
-      // Ler a entrada de um número pelo console
-      Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+  /**
+   * Initial Method.
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-      System.out.println("Digite a medida do lado do quadrado:");
-      double lado = scan.nextDouble();
-      
-      // Seu código aqui
+    System.out.println("Digite a medida do lado do quadrado:");
+    double lado = scan.nextDouble();
 
+    double area = lado * lado;
 
-      // Imprime a saída no console. OBS: descomente a próxima linha
-      //System.out.println("A area do quadrado de lado "+ lado + " é igual a " + area);
-   }
+    System.out.println("A area do quadrado de lado " + lado + " é igual a " + area);
+    scan.close();
+  }
 }
